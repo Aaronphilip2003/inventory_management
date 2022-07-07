9c7ea0309c6c661b87efa4464054207eb9da76cf
Aaron Philip <aaronphilip2003@gmail.com>
2022-07-07 15:57:50 +0530

    Database creation completed

---
+++ inventory.py
@@ -0,0 +1,22 @@
+from tkinter import *
+import sqlite3
+
+conn=sqlite3.connect("people.db")
+cursor=conn.cursor()
+
+cursor.execute("SELECT * FROM names")
+table=cursor.fetchall()
+
+list_names=[]
+
+for row in table:
+    list_names.append(row[0])
+print(list_names)
+
+conn.close()
+
+for i in list_names:
+    conn=sqlite3.connect(f"{i}.db")
+    cursor=conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS equipment_issued(equipment_name)")
+    conn.close()
